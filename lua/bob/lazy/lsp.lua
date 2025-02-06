e6c842224ed7e904b25bb860a9feb71ada4f5d74
bob <bob@bob>
2025-02-06 16:49:58 +0800

    added null-ls

--- lua/bob/lazy/lsp.lua
+++ lua/bob/lazy/lsp.lua
@@ -9,30 +9,49 @@ return {
                         "hrsh7th/cmp-nvim-lsp",
                 },
                 config = function()
-
                         require("cmp").setup({
                                 source = {
-                                        {name = 'nvim_lsp'}
-                                }
+                                        { name = "nvim_lsp" },
+                                },
                         })
 
-                        require("mason").setup(                        )
+                        require("mason").setup()
                         require("mason-lspconfig").setup({
                                 ensure_installed = {
                                         "lua_ls",
                                         "pyright",
-                                }
+                                },
                         })
 
-
-                        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+                        local lspconfig_defaults = require("lspconfig").util.default_config
+                        local cmp_lsp = require("cmp_nvim_lsp")
+                        local capabilities =
+                            vim.tbl_deep_extend("force", {}, lspconfig_defaults.capabilities,
+                                    cmp_lsp.default_capabilities())
 
                         require("lspconfig").lua_ls.setup({
-                                on_attach = lsp_attach,
                                 capabilities = capabilities,
                         })
-                        require("lspconfig").pyright.setup{}
-                end,
+                        require("lspconfig").pyright.setup({
+                                capabilities = capabilities,
+                        })
+                        local cmp = require('cmp')
 
-        }
+                        cmp.setup({
+                                sources = {
+                                        { name = "nvim_lsp" },
+                                },
+                                snippet = {
+                                        expand = function(args)
+                                                -- You need Neovim v0.10 to use vim.snippet
+                                                vim.snippet.expand(args.body)
+                                        end,
+                                },
+                                mapping = cmp.mapping.preset.insert({}),
+                        })
+                        vim.keymap.set("n", "<leader>fv", function()
+                                vim.lsp.buf.format()
+                        end)
+                end,
+        },
 }
