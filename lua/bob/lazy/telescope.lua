cedb96c66ba9cce897158d8927c095eeafb063ae
leedq <leedq@mail.com>
2025-05-03 10:03:22 +0800

    added keybinds for symbols

--- lua/bob/lazy/telescope.lua
+++ lua/bob/lazy/telescope.lua
@@ -16,7 +16,10 @@ return {
                         vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
                         vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
                         vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+                        vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+                        vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols,  { desc = "Telescope get document symbols." })
                         vim.keymap.set("n", "<leader>fig", function() builtin.live_grep({search_dirs={vim.fn.expand("%:p")}}) end, { desc = "Telescope live grep current buffer." })
+                        vim.keymap.set("n", "<leader>fx", function ()  builtin.diagnostics({bufnr=0}) end , { desc = "Telescope get diagnostics" })
                 end,
         },
         { "nvim-telescope/telescope-ui-select.nvim",
